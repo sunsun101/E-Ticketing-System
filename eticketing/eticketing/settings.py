6aff580acc1b89f2e9c257afd76048c9642ede29
Mufaddal Enayath <st122331@ait.asia>
2022-04-16 02:58:02 +0700

    ali

--- eticketing/eticketing/settings.py
+++ eticketing/eticketing/settings.py
@@ -153,14 +153,18 @@ USE_I18N = True
 USE_TZ = True
 
 # SECURE_SSL_REDIRECT = False
-CSRF_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
