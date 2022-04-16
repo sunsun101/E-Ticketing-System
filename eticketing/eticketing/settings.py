fc12b05b2b36e96db08237b389fa937b992095cb
Mufaddal Enayath <st122331@ait.asia>
2022-04-16 23:43:57 +0700

    update django app for production

--- eticketing/eticketing/settings.py
+++ eticketing/eticketing/settings.py
@@ -11,14 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 ENV_FILE_PATH = BASE_DIR / ".env"
-
-dotenv.read_dotenv(str(ENV_FILE_PATH))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -28,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
-
+SESSION_COOKIE_SECURE =True
 ENV_ALLOWED_HOST = os.environ.get("ENV_ALLOWED_HOST")
 ALLOWED_HOSTS = ['*']
 # if ENV_ALLOWED_HOST:
@@ -153,7 +150,7 @@ USE_I18N = True
 USE_TZ = True
 
 # SECURE_SSL_REDIRECT = False
-# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
