105bfacfb9b4a8825c629a410850c41d1d9a3a2f
Mufaddal Enayath <st122331@ait.asia>
2022-04-19 00:33:18 +0700

    prepare db on server

--- eticketing/eticketing/settings.py
+++ eticketing/eticketing/settings.py
@@ -20,7 +20,7 @@ ENV_FILE_PATH = BASE_DIR / ".env"
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","fill_later")
 # SECRET_KEY = "fix_this_later"
 
 # SECURITY WARNING: don't run with debug turned on in production!
