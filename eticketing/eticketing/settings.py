fe737d226c223bc3b15134c49f6f4563158bec56
Sunsun Kasajoo <st122283@ait.asia>
2022-04-19 02:18:45 +0700

    static file location fix

--- eticketing/eticketing/settings.py
+++ eticketing/eticketing/settings.py
@@ -157,8 +157,8 @@ CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'eticketing/static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'eticketing/static')
+STATIC_ROOT = BASE_DIR / 'new/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'new/static')
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
